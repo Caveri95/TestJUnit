655bcae69fad805153edaf93dc4d61a8e7d69535
helloworld <ura22051995@gmail.com>
2023-04-11 11:07:19 +0300

    Первые тесты

--- src/main/java/com/example/testjunit/TestJUnitApplication.java
+++ src/main/java/com/example/testjunit/TestJUnitApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestJUnitApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TestJUnitApplication.class, args);
     }
+
+
+
+
+
 
 }
